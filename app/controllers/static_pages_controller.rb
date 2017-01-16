a8bc3c50fd248466b2d0b5499490b8bfdffb7844
castengo <catalina.astengo@hotmail.com>
2017-01-15 17:35:20 -0700

    Added query to get models by make

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,48 @@
 class StaticPagesController < ApplicationController
+  before_action :start_edmund
+
+  attr_accessor :edmund
 
   def index
-    # makes = JSON.parse(Edmund.new.tco_makes.body)["makes"]
-    makes = JSON.parse(File.read("db/makes.json"))["makes"]
-    # options for select needs to be array of arrays in format [name, value]
+    # makes = JSON.parse(edmund.tco_makes.body)["makes"]
+    makes_json = JSON.parse(File.read("db/makes.json"))["makes"]
+    makes = {}
     options = []
-    makes.each do |make|
-      options << [make[1]["name"], make[1]["id"]]
+    makes_json.each do |make|
+      makes[make[0]] = make[1]
+      options << [ make[1]["name"], make[1]["id"] ]
     end
     @make_options = options
+  end
+  # @test = JSON.parse(edmund.tco_models('200000001').body)["models"]
+
+  # response
+  # "a3:Sedan"=>
+  # {"id"=>"Audi_A3",
+  #  "name"=>"A3 Sedan",
+  #  "submodel"=>"Sedan",
+  #  "nicesubmodel"=>"sedan",
+  #  "model"=>"A3",
+  #  "nicemodel"=>"a3",
+  #  "link"=>"/audi/a3",
+  #  "years"=>{"NEW"=>[2017], "NEW_USED"=>[2016], "USED"=>[2015]}},
+  def models
+    models = JSON.parse(edmund.tco_models(params[:make_id]).body)["makes"]
+    options = []
+
+    model.each do |model|
+      nil
+    end
+    @model_options = nil
+  end
+
+  # https://api.edmunds.com/api/vehicle/v2/honda/civic?state=used&fmt=json&api_key={api key}
+  def styles
+  end
+
+  private
+
+  def start_edmund
+    @edmund = Edmund.new
   end
 end
--- app/models/edmund.rb
+++ app/models/edmund.rb
@@ -1,10 +1,20 @@
 class Edmund
   include HTTParty
-  base_uri 'http://api.edmunds.com/v1/api/tco'
+  base_uri 'http://api.edmunds.com'
   default_params fmt: 'json', api_key: ENV['EDMUND_API_KEY']
 
   # http://api.edmunds.com/v1/api/tco/getmakeswithtcodata?api_key=xxx&fmt=json
   def tco_makes
-    self.class.get('/getmakeswithtcodata?')
+    self.class.get('/v1/api/tco/getmakeswithtcodata?')
   end
+
+  # https://api.edmunds.com/v1/api/tco/getmodelswithtcodata?makeid=200000001&fmt=json&api_key={api key}
+  def tco_models(make_id)
+    self.class.get('/v1/api/tco/getmodelswithtcodata?', { query: { makeid: make_id} })
+  end
+
+  # https://api.edmunds.com/api/vehicle/v2/honda/civic?state=used&fmt=json&api_key={api key}
+  # def tco_styles(make_id, model)
+  #   self.class.get("/api/vehicle/v2/#{make_id.downcase}/civic?")
+  # end
 end
