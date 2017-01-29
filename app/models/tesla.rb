e1c1cfa1f680cadca969c4e80d2ef27d79cd1baf
castengo <catalina.astengo@hotmail.com>
2017-01-29 09:30:42 -0700

    Financing only calculated on create

--- app/models/tesla.rb
+++ app/models/tesla.rb
@@ -1,12 +1,15 @@
 class Tesla < ActiveRecord::Base
 
-  before_save :calculate_financing, :update_total
+  before_save :update_total
+  before_create :calculate_financing
 
   validates_presence_of :model, :trim, :price
 
   def calculate_financing
-    loan_amount = self.price * BigDecimal.new("0.9")
-    self.financing = LoanCalculator.new(principal: loan_amount).total_interest
+    if self.financing < BigDecimal.new("0.50")
+      loan_amount = self.price * BigDecimal.new("0.9")
+      self.financing = LoanCalculator.new(principal: loan_amount).total_interest
+    end
   end
 
   def update_total
